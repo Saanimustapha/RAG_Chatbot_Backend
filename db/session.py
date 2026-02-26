b6ec47a4a4659d5eb5193351de684a98ed609805
Saani Mustapha Deishini <deishini2003@gmail.com>
2026-02-26 13:57:51 +0000

    Added minor engine configuration to handle client text encoding

--- db/session.py
+++ db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from RAG_Chatbot_Backend.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, pool_pre_ping=True)
+engine = create_async_engine(settings.DATABASE_URL, pool_pre_ping=True, connect_args={"server_settings": {"client_encoding": "UTF8"}})
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_db():
